28404d2762ca0677c30a4a8eb2a66e1ebf9405a3
ywk94 <yangwenkai94@163.com>
2018-09-29 09:14:01 +0800

    创建管理对象和添加迁移指令

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_session import Session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 class Config(object):
@@ -55,6 +57,15 @@ csrf = CSRFProtect(app)
 # 5.创建session对象,将session的储存方法进行调整(flask后端内存-->redis数据库)
 Session(app)
 
+# 6.创建管理对象
+manager = Manager(app)
+
+# 7.数据库迁移对象
+Migrate(app, db)
+
+# 8.数据库迁移指令
+manager.add_command("db", MigrateCommand)
+
 
 @app.route('/index')
 def index():
@@ -62,4 +73,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
