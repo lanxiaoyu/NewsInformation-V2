0b02db9849bc88e907efec2d08daa0d455918236
ywk94 <yangwenkai94@163.com>
2018-09-29 08:56:15 +0800

    创建redis对象和redis数据库配置

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
 
 class Config(object):
@@ -8,6 +9,11 @@ class Config(object):
     # mysql数据库的配置信息
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/NewsInformation_v2"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis数据库的配置信息
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+    REDIS_NUM = 1
 
 
 # 1.创建app对象
@@ -19,6 +25,9 @@ app.config.from_object(Config)
 # 2.创建数据库对象
 db = SQLAlchemy(app)
 
+# 3.创建redis数据库对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=Config.REDIS_NUM)
+
 
 @app.route('/index')
 def index():
@@ -26,4 +35,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
