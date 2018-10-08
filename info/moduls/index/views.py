93e25c1ea243fb2ad3bc8bcf0bfb3569eb6a3827
ywk94 <yangwenkai94@163.com>
2018-10-08 20:42:44 +0800

    新闻列表数据后段实现(未完成)

--- info/moduls/index/views.py
+++ info/moduls/index/views.py
@@ -2,10 +2,67 @@ from info import redis_store, constants
 from info.utils.response_code import RET
 from . import index_bp
 from info.models import User, News, Category
-from flask import render_template, current_app, session, jsonify
+from flask import render_template, current_app, session, jsonify, request
 
 
-# 2.使用蓝图对象
+@index_bp.route('/news_list')
+def get_news_list():
+    """获取新闻列表数据"""
+    """
+    1.获取参数
+        1.1 cid :分类id    page:当前页码(默认第一页)   per_page:每页的多少条数据(默认10)
+    2.检验参数
+        2.1 非空判断
+        2.2 整型强制转换
+    3.逻辑处理
+        3.1 分页查询
+        3.2 对象列表转成字典列表
+    4.返回值
+    """
+    # 1.1
+    param_dict = request.args
+
+    cid = param_dict.get("cid")
+    page = param_dict.get("page", '1')
+    per_page = param_dict.get("per_page", "10")
+
+    # 2.1 非空判断
+    if not cid:
+        return jsonify({"errno": RET.PARAMERR, "errmsg": '参数不足'})
+
+    # 2.2
+    cid = int(cid)
+    page = int(page)
+    per_page = int(per_page)
+
+    # 3.1
+    try:
+        paginate = News.query.filter(News.category_id ==cid).order_by(News.create_time.desc()).paginate(page,per_page,False)
+        # 获取当前页面的所有数据
+        news_list = paginate.items
+        # 获取当前页码
+        current_page = paginate.page
+        # 获取总页数
+        total_page = paginate.pages
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify({"errno":RET.DBERR, "errmsg": '查询新闻列表数据异常'})
+
+    # 3.2
+    news_dict_list = []
+    for news in news_list if news_list else []:
+        news_dict_list.append(news.to_dict())
+
+    data = {
+        "news_list":news_dict_list,
+        "current_page":current_page,
+        "total_page":total_page
+    }
+
+    # 4
+    return jsonify({"errno":RET.OK, "errmsg": 'ok'},data = data)
+
 
 @index_bp.route('/')
 def index():
@@ -13,7 +70,7 @@ def index():
     # 1. 获取当前登录用户的id
     user_id = session.get("user_id")
 
-    user = None #type:User
+    user = None  # type:User
 
     # 2.查询用户对象
     if user_id:
@@ -22,7 +79,7 @@ def index():
             user = User.query.get(user_id)
         except Exception as e:
             current_app.logger.error(e)
-            return jsonify({"errno":RET.DBERR, "errmsg": '数据库查询用户错误'})
+            return jsonify({"errno": RET.DBERR, "errmsg": '数据库查询用户错误'})
 
     # ------------------获取新闻点击排行数据------------------
     try:
@@ -30,7 +87,7 @@ def index():
 
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify({"errno":RET.DBERR, "errmsg": '数据库获取新闻错误'})
+        return jsonify({"errno": RET.DBERR, "errmsg": '数据库获取新闻错误'})
 
     # 字典列表初始化
     """
@@ -45,31 +102,29 @@ def index():
         # 构建字典列表
         news_rank_dict_list.append(news_dict)
 
-    #----------获取新闻分类数据-----------
+    # ----------获取新闻分类数据-----------
     try:
         categories = Category.query.all()
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify({"errno":RET.DBERR, "errmsg": '数据库查询类别错误'})
+        return jsonify({"errno": RET.DBERR, "errmsg": '数据库查询类别错误'})
 
     # 对象列表转换成字典列表
     category_dict_list = []
     for category in categories if categories else []:
         category_dict_list.append(category.to_dict())
 
-
     # 4. 组织响应数据字典
     data = {
-        "user_info":user.to_dict() if user else None,
-        "news_rank_list":news_rank_dict_list,
-        "categories":category_dict_list
+        "user_info": user.to_dict() if user else None,
+        "news_rank_list": news_rank_dict_list,
+        "categories": category_dict_list
     }
 
-    return render_template("news/index.html",data = data)
+    return render_template("news/index.html", data=data)
 
 
 @index_bp.route("/favicon.ico")
 def favicon():
     """返回网页图标"""
     return current_app.send_static_file("news/favicon.ico")
-
