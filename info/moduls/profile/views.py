d35fd985a39d95af60773ee485795be704f946be
ywk94 <yangwenkai94@163.com>
2018-10-12 13:37:11 +0800

    用户发布的新闻列表前后端逻辑

--- info/moduls/profile/views.py
+++ info/moduls/profile/views.py
@@ -8,6 +8,70 @@ from info.utils.pic_storage import pic_storage
 from info.models import Category
 
 
+# /user/news_list?p=页码
+@profile_bp.route('/news_list')
+@user_login_data
+def news_list():
+    """获取当前用户新闻收藏列表数据"""
+
+    """
+    1.获取参数
+        1.1  user :当前用户对象, p:当前页码(默认值第一页)
+    2.校验参数
+        2.1 非空判断
+    3.逻辑处理
+        3.0 进行分页查询
+    4.返回值
+    """
+    # 获取用户对象
+    user = g.user
+    p = request.args.get('p', 1)
+    # 2.1参数类型判断
+    try:
+        p = int(p)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify({"errno": RET.PARAMERR, "errmsg": '参数内容错误'})
+    # 当前用户存在的时候采取查询
+    """
+    对比:
+    当前用户收藏的新闻查询: user.collection_news
+    当前用户发布的新闻查询: News.query.filter(News.user_id == user.id)
+    """
+
+
+    news_list = []
+    current_page = 1
+    total_page = 1
+    if user:
+        try:
+            paginate = News.query.filter(News.user_id == user.id).paginate(p, constants.OTHER_NEWS_PAGE_MAX_COUNT, False)
+            # 当前页码所有数据
+            news_list = paginate.items
+            # 当前页码
+            current_page = paginate.page
+            # 总页数
+            total_page = paginate.pages
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(errno=RET.DBERR, errmsg="查询发布新闻分页数据异常")
+
+    # 对象列表转字典列表
+    news_dict_list = []
+    for news in news_list if news_list else []:
+        news_dict_list.append(news.to_basic_dict())
+
+    # 组织响应数据
+    data = {
+        "news_list": news_dict_list,
+        "current_page": current_page,
+        "total_page": total_page
+    }
+
+    # 4.返回值
+    return render_template("profile/user_news_list.html", data=data)
+
+
 @profile_bp.route('/news_release', methods=['GET', 'POST'])
 @user_login_data
 def news_release():
@@ -27,18 +91,18 @@ def news_release():
         for category in categories if categories else []:
             category_dict_list.append(category.to_dict())
 
-            #注意: 移除最新分类
+            # 注意: 移除最新分类
         category_dict_list.pop(0)
 
-        #组织响应数据
+        # 组织响应数据
         data = {
-            "categories":category_dict_list
+            "categories": category_dict_list
         }
         return render_template("profile/user_news_release.html")
 
     # /user/collection?p=页码
 
-    #POST请求:发布新闻
+    # POST请求:发布新闻
     """
     1.获取参数
         1.1 title:新闻标题,category_id:新闻分类id   digest:新闻摘要,
@@ -53,7 +117,7 @@ def news_release():
     4.返回值
     """
 
-    #1.1 获取参数
+    # 1.1 获取参数
     title = request.form.get("title")
     category_id = request.form.get("category_id")
     digest = request.form.get("digest")
@@ -61,27 +125,27 @@ def news_release():
     index_image = request.form.get("index_image")
     source = "个人发布"
 
-    #2.1 非空判断
+    # 2.1 非空判断
 
     if not all([title, category_id, digest, content, index_image]):
         return jsonify(errno=RET.PARAMERR, errmsg="参数不足")
 
-    #2.2 用户是否登录判断
+    # 2.2 用户是否登录判断
     if not user:
         return jsonify(errno=RET.SESSIONERR, errmsg='用户未登录')
     try:
         pic_data = index_image.read()
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify({"errno":RET.PARAMERR, "errmsg": '数据库不能为空'})
+        return jsonify({"errno": RET.PARAMERR, "errmsg": '数据库不能为空'})
 
-    #3.0 将新闻主图片上传七牛云
+    # 3.0 将新闻主图片上传七牛云
     try:
         pic_name = pic_storage(pic_data)
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify({"errno":RET.THIRDERR, "errmsg": '上传图片到七牛云失败'})
-    #3.1创建新闻对象,并将其属性赋值(8个属性0
+        return jsonify({"errno": RET.THIRDERR, "errmsg": '上传图片到七牛云失败'})
+    # 3.1创建新闻对象,并将其属性赋值(8个属性0
     news = News()
     news.title = title
     news.category_id = category_id
@@ -90,7 +154,7 @@ def news_release():
     news.index_image_url = constants.QINIU_DOMIN_PREFIX + pic_name
     news.source = source
     news.user_id = user.id
-    #设置新闻发布后的状态为:审核中
+    # 设置新闻发布后的状态为:审核中
     news.status = 1
     # 3.2 保存回到数据库
     try:
@@ -99,12 +163,9 @@ def news_release():
     except Exception as e:
         current_app.logger.error(e)
         db.session.rollback()
-        return jsonify({"errno":RET.DBERR, "errmsg": '保存新闻对象异常'})
+        return jsonify({"errno": RET.DBERR, "errmsg": '保存新闻对象异常'})
     # 4. 返回值
     return jsonify(errno=RET.OK, errmsg='发布新闻成功')
-
-
-
 
 
 @profile_bp.route('/collection')
@@ -152,7 +213,7 @@ def user_collection_news():
 
         except Exception as e:
             current_app.logger.error(e)
-            return jsonify({"errno": RET.DBERR, "errmsg": ''})
+            return jsonify({"errno": RET.DBERR, "errmsg": '查询收藏新闻分页数据异常'})
     # 对象列表转字典列表
     news_dict_collections = []
     for news in news_collections if news_collections else []:
